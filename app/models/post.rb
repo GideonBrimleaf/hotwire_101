ed61fb29e386f9667eaa4f02d4d6d9cf2b0ec117
chrisdrydenaltmetric <chris@altmetric.com>
2023-09-01 14:46:18 +0100

    Can stream posts to posts index page

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,7 @@ class Post < ApplicationRecord
   has_rich_text :content
 
   has_many :comments, dependent: :destroy
+  after_create_commit -> { broadcast_prepend_to "posts" }
+  after_update_commit -> { broadcast_replace_to "posts" }
+  after_destroy_commit -> { broadcast_remove_to "posts" }
 end
